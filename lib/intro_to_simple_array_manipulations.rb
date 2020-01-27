0d5ef4819d0acf242b36b1f034d3b23fca179f60
Alessandro Duterte <alexduterte@gmail.com>
2020-01-27 17:38:32 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_shift(array, string)
+  array.unshift(string)
+end
+
+def using_pop
+  array.pop
+end
+
+def pop_with_args
+  array.pop(2)
+end
+
+def using_shift
+  array.shift
+end
+
+def shift_with_args
+  array.shift(2)
+end
+
+def using_concat
+  all_my_favs.concat(my_favorite_things, more_favs)
+end
